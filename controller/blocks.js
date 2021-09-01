76fa0f5e5feee9822f43f163c2afada2d12b4efe
cr0c0pk1 <alpics1337@gmail.com>
2021-09-01 16:47:25 +0300

    fix: get block by hash from C-chain

--- controller/blocks.js
+++ controller/blocks.js
@@ -1,32 +1,7 @@
-var request = require("request");
 
 const axios = require("axios");
 
-const headers = {
-    "content-type": "text/plain"
-};
-
-// exports.getBlockByHash = (req, res, next) => {
-//     var dataString = `{"jsonrpc":"2.0","method":"eth_getBlockByHash","params":["${req.params.hash}", true]}, "id":1`;
-//     var options = {
-//         url: "http://192.168.0.87:9650/ext/bc/C/rpc",
-//         method: "POST",
-//         headers: headers,
-//         body: dataString
-//     };
-
-//     callback = (error, response, body) => {
-//         console.log(JSON.parse(body));
-//         if (!error && response.statusCode == 200) {
-//           const data = JSON.parse(body);
-//           console.log("data: ", data);
-//           res.send(data);
-//         }
-//     };
-
-//     request(options, callback);
-// };
-
+//get block by hash from C-chain
 exports.getBlockByHash = async (req, res, next) => {
     await axios.post('http://192.168.0.87:9650/ext/bc/C/rpc', {
         jsonrpc: '2.0',
@@ -41,7 +16,6 @@ exports.getBlockByHash = async (req, res, next) => {
     })
     .then((response) => {
         console.log(response.data);
-        // const data = JSON.parse(response);
         res.send(response.data);
     }, (error) => {
         console.log(error);
