61430603a70586a9695859da1cd810a27db5ea26
Ahmet <ahmedovv123@gmail.com>
2021-10-07 14:59:53 +0300

    feat: add API tests

--- __tests__/tests.spec.js
+++ __tests__/tests.spec.js
@@ -26,6 +26,40 @@ describe('GET /address/hash/{hash}', () => {
     });
 });
 
+describe('GET /blocks/hash/{hash}', () => {
+    it('should satisfy OpenAPI spec', async () => {
+        const hash = '0x0bcd0c4e5635f21dd4352aa82692a5e29bcf2c5373da9427e5ab38bd4c7cfd33'
+        const res = await axios.get(`http://localhost:4444/blocks/hash/${hash}`)
+
+        expect(res.status).toEqual(200);
+        expect(res).toSatisfyApiSpec();
+       
+    });
+});
+
+describe('GET /blocks/number/{blockNumber}', () => {
+    it('should satisfy OpenAPI spec', async () => {
+        const blockNumber = 1940150;
+        const res = await axios.get(`http://localhost:4444/blocks/number/${blockNumber}`)
+
+        expect(res.status).toEqual(200);
+        expect(res).toSatisfyApiSpec();
+       
+    });
+});
+
+describe('GET /transactions/hash/{hash}', () => {
+    it('should satisfy OpenAPI spec', async () => {
+        const hash = '0x118e1747566adeaab6afede9de76ebeb5b10bb56ec510a099fb5a82221e9d0e7';
+        const res = await axios.get(`http://localhost:4444/transactions/hash/${hash}`)
+
+        expect(res.status).toEqual(200);
+        expect(res).toSatisfyApiSpec();
+       
+    });
+});
+
+
 describe('C-chain', () => {
     it('test getblockbyhash without starting client', async () => {
         //const controller = new blocksController();
