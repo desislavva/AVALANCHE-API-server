b9944a47bf82ce3f91acbfb9d6351008b4e1d765
Ahmet <ahmedovv123@gmail.com>
2021-10-07 12:13:04 +0300

    feat: automate Api tests

--- __tests__/tests.spec.js
+++ __tests__/tests.spec.js
@@ -1,4 +1,30 @@
 const cChainMethods = require('../services/c-chain');
+const jestOpenAPI = require('jest-openapi').default;
+const { default: axios } = require('axios');
+const path = require('path')
+
+jestOpenAPI(path.join(__dirname, '../openapi/openapi.yml'));
+
+describe('GET /network', () => {
+    it('should satisfy OpenAPI spec', async () => {
+        const res = await axios.get('http://localhost:4444/network')
+
+        expect(res.status).toEqual(200);
+        expect(res).toSatisfyApiSpec();
+       
+    });
+});
+
+describe('GET /address/hash/{hash}', () => {
+    it('should satisfy OpenAPI spec', async () => {
+        const hash = 'X-fuji1xpmx0ljrpvqexrvrj26fnggvr0ax9wm32gaxmx'
+        const res = await axios.get(`http://localhost:4444/address/hash/${hash}`)
+
+        expect(res.status).toEqual(200);
+        expect(res).toSatisfyApiSpec();
+       
+    });
+});
 
 describe('C-chain', () => {
     it('test getblockbyhash without starting client', async () => {
